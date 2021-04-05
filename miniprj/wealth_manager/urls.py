cce8babbf60da332193b09ea483ca7183209eb25
Divyam Prajapati <divyambp@gmail.com>
2021-04-05 18:58:33 +0530

    All pages files added

--- miniprj/wealth_manager/urls.py
+++ miniprj/wealth_manager/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("Expense/", views.expense, name="expense"),
+    path("Income/", views.income, name="income"),
+    path("BankAccount/", views.bankAccount, name="bankAccount"),
+    path("Budget/", views.budget, name="budget"),
+    path("Savings/", views.savings, name="savings"),
+    path("Profile/", views.profile, name="profile"),
+    path("Help/", views.help, name="help"),
+    path("Logout/", views.logout, name="logout"),
 ]
--- miniprj/wealth_manager/views.py
+++ miniprj/wealth_manager/views.py
@@ -1,6 +1,29 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "manager/dashboard.html")
+    return render(request, "manager/dashboard.html", {'nbar': 'home'})
+
+def expense(request): 
+    return render(request, "manager/expense.html", {'nbar': 'expense'})
+
+def income(request): 
+    return render(request, "manager/income.html", {'nbar': 'income'})
+
+def bankAccount(request): 
+    return render(request, "manager/income.html", {'nbar': 'bankAccount'})
+
+def budget(request): 
+    return render(request, "manager/income.html", {'nbar': 'budget'})
+
+def savings(request): 
+    return render(request, "manager/income.html", {'nbar': 'savings'})
+
+def profile(request): 
+    return render(request, "manager/income.html", {'nbar': 'profile'})
+
+def help(request): 
+    return render(request, "manager/income.html", {'nbar': 'help'})
+
+def logout(request): 
+    return render(request, "manager/income.html", {'nbar': 'logout'})
